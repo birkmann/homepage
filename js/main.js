530ac00795f5f7e86d25d8565fc39e2a4167ba6a
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-11-26 20:17:52 +0100

    files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.querySelector('html').classList.remove('no-js');
 
 window.onload = function(){
-		document.querySelector('body').classList.add('loaded');
+	document.querySelector('body').classList.add('loaded');
 }, 250;
